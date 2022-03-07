a4e2fc13f2a739851fd557978421a7653901639d
726a67 <726a67@gmail.com>
2022-03-07 09:25:58 -0600

    add metadata/updateinfo logic to rhel7-repo-sync

--- rhel7-repo-sync/root/usr/local/bin/rhel7-repo-sync.bash
+++ rhel7-repo-sync/root/usr/local/bin/rhel7-repo-sync.bash
@@ -10,8 +10,11 @@ for REPO in $(grep -v "#" ${REPO_FILE})
 do
 	echo [${REPO}] && \
 	echo -e && \
-	reposync --plugins --download_path=${EXPORT_DIR} --downloadcomps --delete --repoid=${REPO} && \
+	reposync --plugins --download_path=${EXPORT_DIR} --downloadcomps --download-metadata --delete --repoid=${REPO} && \
 	if [ -f ${EXPORT_DIR}/${REPO}/comps.xml ]; then createrepo ${EXPORT_DIR}/${REPO} --groupfile comps.xml && rm ${EXPORT_DIR}/${REPO}/comps.xml; \
 	else createrepo ${EXPORT_DIR}/${REPO}; fi && \
+	if [ -f ${EXPORT_DIR}/${REPO}/*-updateinfo.xml.gz ]; then rm -rf ${EXPORT_DIR}/${REPO}/repodata/*-updateinfo.xml.gz && zcat ${EXPORT_DIR}/${REPO}/*-updateinfo.xml.gz > ${EXPORT_DIR}/${REPO}/repodata/updateinfo.xml && \
+	modifyrepo ${EXPORT_DIR}/${REPO}/repodata/updateinfo.xml ${EXPORT_DIR}/${REPO}/repodata/ && rm ${EXPORT_DIR}/${REPO}/*-updateinfo.xml.gz; fi && \
+	find ${EXPORT_DIR}/${REPO} -maxdepth 1 -type f -exec rm -f {} \; && \
 	echo -e
 done
